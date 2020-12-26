ac40e82545caebec065722e72aa4a4e225a4465d
Carlos Padilla <mr.carlos.padilla@gmail.com>
2020-12-25 22:21:30 -0600

    practicing exercises

--- js/external.js
+++ js/external.js
@@ -9,3 +9,16 @@ var brotherBear = parseFloat(prompt("How many days do you want to rent Brother B
 var hercules = parseFloat(prompt("How many days do you want to rent Hercules?"));
 var pricePerDay = 3
 var total = alert("Your total is: $" + ((littleMermaid + brotherBear + hercules) * pricePerDay)).toFixed(2);
+
+var googlePay = parseInt(prompt("How much does Google pay per hour?"));
+var amazonPay = parseInt(prompt("How much does Amazon pay per hour?"));
+var facebookPay = parseInt(prompt("How much does Facebook pay per hour?"));
+var googleHoursWorked = parseInt(prompt("How many hours did you work at Google?"));
+var amazonHoursWorked = parseInt(prompt("How many hours did you work at Amazon?"));
+var facebookHoursWorked = parseInt(prompt("How many hours did you work at Facebook?"));
+var googleTotalPay = googlePay * googleHoursWorked;
+var amazonTotalPay = amazonPay * amazonHoursWorked;
+var facebookTotalPay = facebookPay * facebookHoursWorked;
+var totalPay = googleTotalPay + amazonTotalPay + facebookTotalPay;
+
+alert("You made a total of $" + totalPay + " this week working at all three companies!");
