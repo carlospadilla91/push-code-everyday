b5b2e2ff245491d04688cfd69d61455fa9cde1de
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-01-04 22:01:37 -0600

    additional arrays practice

---
+++ js/arrays_practice.js
@@ -0,0 +1,32 @@
+"use strict"
+
+// Define a function named allIndexesOf that takes in two arguments.
+// The first argument should be the array to search and the second argument should be the value you want to search for.
+// If the item does not exist in the provided array, return an empty array.
+//
+//     Given:
+//
+var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+
+function allIndexesOf(array, value) {
+    return array.lastIndexOf(value);
+}
+
+console.log(allIndexesOf(fruits, "apple")); //should return the array [0, 3]
+console.log(allIndexesOf(fruits, "guava")); //should return the array []
+console.log(allIndexesOf(fruits, "pineapple")); //should return [4]
+
+// Define a function named removeAll(array, value) that takes in two arguments.
+// The first argument should be an array and the second argument should be a value you wish to remove
+
+// Given:
+
+var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+
+function removeAll(array, value) {
+
+}
+
+removeAll(bugs, "ant") //should return ["mosquito", "scorpion",   "mosquito", "typo", "reference error", "type error"]
+removeAll(bugs, "mosquito") //should return ["ant", "scorpion", "ant",   "ant", "typo", "reference error", "type error"]
+removeAll(bugs, "roach") //should return the original array b/c "roach" has no occurrances.
